03ff22a9a6490674f01bd58da707d51bac3de862
AmrutaDanannavar <97725582+AmrutaDanannavar@users.noreply.github.com>
2023-10-19 13:06:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// for dice 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage="dice" + randomNumber1 + ".png";
+var randomImageSource= "" + randomDiceImage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+//for dice 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2="dice" + randomNumber2 + ".png";
+var randomImageSource2= "" + randomDiceImage2;
+var image2= document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+//for text changing
+ if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins  🚩";
+    document.querySelector("h1").style.color="blueviolet";
+ }
+ else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins   🚩";
+    document.querySelector("h1").style.color="chocolate";
+ }
+ else{
+    document.querySelector("h1").innerHTML="Draw  😐";
+ }
+
+
+//var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+//var randomImageSource2= "images/dice" + randomNumber2 + ".png";
+ //document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
